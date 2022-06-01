f3c319a56748ecb73959a328ed5b7b52c43d3fbc
Zhasulan <zhasulanmukishevab@gmail.com>
2022-06-01 14:25:02 +0600

    Реализован класс для пользователей

---
+++ src/main/java/kz/mukysh/task_3/models/User.java
@@ -0,0 +1,27 @@
+package kz.mukysh.task_3.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column
+    private String address;
+
+}
